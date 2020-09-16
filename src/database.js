c8a4b8cff2246e3d43846e276dddb4a780b55af4
overvyn <overvyn@gmail.com>
2020-09-16 17:07:38 -0300

    blank user

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect( 'mongodb+srv://lpb:Manager01@clustermongo0.0rzrt.gcp.mongodb.net/chat?retryWrites=true&w=majority', {
+mongoose.connect( 'mongodb+srv://user:password@clustermongo0.0rzrt.gcp.mongodb.net/chat?retryWrites=true&w=majority', {
     useNewUrlParser: true
 })
   .then(db => console.log('db connected'))
